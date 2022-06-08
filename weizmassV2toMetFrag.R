f5e56d059243bcee1cfe9c143dab85f84023fbcd
Steffen Neumann <sneumann@ipb-halle.de>
2022-06-08 17:14:35 +0200

    started git

---
+++ weizmassV2toMetFrag.R
@@ -0,0 +1,90 @@
+## devtools::install_github("rformassspectrometry/MsBackendMsp")
+library(Spectra)
+library(MsCoreUtils)
+library(MsBackendMS)
+library(metfRag)
+library(IRanges)
+
+#source("functions-ms.R")
+
+setwd("/home/sneumann/src/weizfrag")
+be <- MsBackendMS()
+
+## Read the CHemistry Metadata
+metadata <- read.delim("msExports/WeizMassV2.tsv", stringsAsFactors=F)
+ion_mode <- metadata[, "ionization"]
+cpdid    <- metadata[, "compound_id"]
+filename <- paste("msExports/", cpdid, ".", tolower(ion_mode), ".ms", sep="")
+
+## Remove the ones where I deleted the *.ms lacking MS2 
+fileexists <- file.exists(filename)
+metadata <- metadata[fileexists, ]
+ion_mode <- metadata[, "ionization"]
+cpdid    <- metadata[, "compound_id"]
+filename <- paste("msExports/", cpdid, ".", tolower(ion_mode), ".ms", sep="")
+
+## Extract metadata
+inchi <- metadata[, "InChI"]
+inchikey <- metadata[, "InChiKey"]
+smiles <- metadata[, "Smiles"]
+
+
+
+#ms <- lapply(filename, readMS)
+ms <- backendInitialize(be, filename)
+
+
+
+options(java.parameters = c("-Xms512m", "-Xmx1024m"))
+library(rcdk)
+# library(fingerprint)
+sp <- get.smiles.parser()
+
+fps <- sapply(smiles, function(s) {
+  m <- parse.smiles(s, smiles.parser = sp)[[1]]
+  fp <- ""
+  if (!is.null(m) && class(m) == "jobjRef" && m@jclass == "org/openscience/cdk/interfaces/IAtomContainer") {
+    fp <- calculateFingerprintFromSmiles(s)
+  } else {
+    cat("Broken SMILES: ", s, "\n")
+    fp <- ""
+  }
+})
+
+smilesOK <- sapply (fps, function(f) f!="")
+len <-  length(which((smilesOK==TRUE)))
+table(smilesOK)
+
+inchikeyOK <- inchikey!=""
+
+sink("/tmp/weizfragV2.mb")
+for (i in seq(1, length(ms))[smilesOK & inchikeyOK]) {
+  if ( (i/len*100)%% 10 == 0) {
+    message(i/len*100, "%")
+  }
+  spec <- ms[i]
+
+  cat(
+    "# SampleName = ", spectraData(spec)[,"compound"], "\n",
+    "# InChI = ", inchi[i], "\n",
+    "# InChIKey = ", inchikey[i], "\n",
+    "# IsPositiveIonMode = ", ifelse(ion_mode[i]=="POSITIVE", "True", "False"), "\n",
+    "# IonizedPrecursorMass = ", spectraData(spec)[,"parentmass"], "\n",
+    "# NumPeaks = ", nrow(peaksData(spec)[[1]]), "\n",
+    "# MolecularFingerPrint = ", fps[i], "\n", sep="")
+   
+    write.table(peaksData(spec), row.names = F, col.names = F)
+    cat("\n")
+}
+sink(NULL)
+
+
+## Create CSV with InChiKey and Fingerprint
+## egrep 'InChIKey|MolecularFingerPrint' MoNA-export-LC-MS-MS_Spectra.mb | cut -d"=" -f 2 | paste -s -d' \n' | sort | uniq  >new.mb.fp 
+
+## Join two of such CSVs to detect (mis)matches
+## join new.mb.fp old.mb.fp | while read I F1 F2 ; do if [ "$F1" == "$F2" ] ; then echo "OK" ; else echo "mismatch: $I" ; echo $F1 ; echo $F2  ; fi ; done 
+
+
+
+
